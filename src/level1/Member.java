3a7b85ff6161499072dc0d814b16c22e76374d30
soheesgit <thgml89567@smail.kognju.ac.kr>
2026-03-31 15:42:23 +0900

    1단계 문제 풀이

--- src/level1/Member.java
+++ src/level1/Member.java
@@ -1,14 +1,14 @@
 package level1;
 
-public class Member {
+public abstract class Member {
 
-    // TODO: 캡슐화에 적절한 접근 제어자를 사용하세요.
-    String name;
-    boolean assignmentSubmitted;
+    private final String name;
+    private boolean assignmentSubmitted;
 
     public Member(String name) {
-        // TODO: name이 null이거나, 빈 문자열이거나, 공백만 있는 문자열이면 IllegalArgumentException 예외를 발생시키세요.
-
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("name이 입력되지 않았습니다.");
+        }
         this.name = name;
         this.assignmentSubmitted = false;
     }
@@ -22,9 +22,16 @@ public class Member {
     }
 
     public void submitAssignment() {
-        // TODO: 과제 제출 로직을 작성하세요.
+        if (isAssignmentSubmitted()) {
+            System.out.println(getName() + "님은 이미 과제를 제출했습니다.");
+        } else {
+            this.assignmentSubmitted = true;
+            System.out.println(getName() + "님이 과제를 제출했습니다.");
+        }
     }
 
     // TODO: getRole(), doTask()를 추상 메서드로 선언하세요.
+    public abstract String getRole();
+    public abstract void doTask();
 
 }
